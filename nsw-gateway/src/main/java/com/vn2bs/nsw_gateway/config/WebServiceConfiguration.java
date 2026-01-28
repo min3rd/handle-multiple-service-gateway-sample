011eb886fcdb7fb9ac4430aaeb5efc769bd3252d
Vũ Văn Minh <min6th@gmail.com>
2026-01-28 17:55:23 +0700

    feat: Add JAXB bindings for BCT and BYT services, implement message endpoint, and update schemas

--- nsw-gateway/src/main/java/com/vn2bs/nsw_gateway/config/WebServiceConfiguration.java
+++ nsw-gateway/src/main/java/com/vn2bs/nsw_gateway/config/WebServiceConfiguration.java
@@ -1,5 +1,6 @@
 package com.vn2bs.nsw_gateway.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -24,7 +25,7 @@ public class WebServiceConfiguration {
     }
 
     @Bean(name = "bct-thu-tuc-1")
-    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema schema) {
+    public DefaultWsdl11Definition BCT_ThuTuc1_defaultWsdl11Definition(@Qualifier("bct-schema") XsdSchema schema) {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("schema");
         wsdl11Definition.setLocationUri("/web-services");
@@ -33,8 +34,26 @@ public class WebServiceConfiguration {
         return wsdl11Definition;
     }
 
+    @Bean(name = "bct-messages")
+    public DefaultWsdl11Definition BCT_Messages_defaultWsdl11Definition(
+            @Qualifier("bct-messages-schema") XsdSchema schema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("BCTMessages");
+        wsdl11Definition.setLocationUri("/web-services");
+        wsdl11Definition.setTargetNamespace("com.vn2bs.webservices.bct.messages");
+        wsdl11Definition.setSchema(schema);
+        return wsdl11Definition;
+    }
+
+    @Bean(name = "bct-messages-schema")
+    @Qualifier("bct-messages-schema")
+    public XsdSchema BCT_Messages_schema() {
+        return new SimpleXsdSchema(new ClassPathResource("xsd/bct/BCTMessages.xsd"));
+    }
+
     @Bean(name = "bct-schema")
-    public XsdSchema schema() {
+    @Qualifier("bct-schema")
+    public XsdSchema BCT_ThucTuc1_schema() {
         return new SimpleXsdSchema(new ClassPathResource("xsd/bct/thutuc1.xsd"));
     }
 }
